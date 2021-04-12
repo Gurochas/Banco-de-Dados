489c30492e6b82554d47020b46d6e146c894c8b0
Gustavo <gurochasouza@gmail.com>
2021-04-12 02:05:08 -0300

    Prova 1 - Lab BD

--- Lab. Banco de Dados/Prova 1/Campeonato.sql
+++ Lab. Banco de Dados/Prova 1/Campeonato.sql
@@ -1,41 +1,57 @@
-USE MASTER 
-go
-DROP DATABASE campeonato
-go
-create database campeonato
+CREATE DATABASE campeonato
 GO
-use campeonato
-GO
+USE campeonato
+
+/*
+Fazer uma aplicação em Java (Web (Servlets) ou Desktop (Swing ou JavaFX)) com SQL Server
+para resolver os problemas, da seguinte maneira: 
+O sistema deve ter 3 tabelas principais:
+
+- Times (Com todos os 20 times)(Não é necessário CRUD para ela)
+	Times (CodigoTime | NomeTime | Cidade | Estadio)
+*/
+
 CREATE TABLE Times(
 	CodigoTime		INT	PRIMARY KEY		IDENTITY,
 	NomeTime		VARCHAR(30)			NOT NULL,
 	Cidade			VARCHAR(30)			NOT NULL,
 	Estadio			VARCHAR(30)			NOT NULL
 )
-GO
-INSERT INTO Times VALUES('Botafogo-SP','RibeirÃ£o Preto','Santa Cruz')
-,('Bragantino','BraganÃ§a Paulista','Nabi Abi Chedid')
-,('Corinthians','SÃ£o Paulo','Arena Corinthians')
-,('FerroviÃ¡ria','Araraquara','Fonte Luminosa')
-,('Guarani','Campinas','Brinco de Ouro da Princesa')
-,('Ituano','Itu','Novelli JÃºnior')
-,('Mirassol','Mirassol','JÃ³se Maria de Campos Maia')
-,('Novorizontino','Novo Horizonte','Jorge Ismael de Biasi')
-,('Oeste','Barueri','Arena Barueri')
-,('Palmeiras','SÃ£o Paulo','Allianz Parque')
-,('Ponte Preta','Campinas','MoisÃ©s Lucarelli')
-,('Red Bull Brasil','Campinas','MoisÃ©s Lucarelli')
-,('Santos','Santos','Vila Belmiro')
-,('SÃ£o Bento','Sorocaba','Walter Ribeiro')
-,('SÃ£o Caetano','SÃ£o Caetano do Sul','Anacletto Campenella')
-,('SÃ£o Paulo','SÃ£o Paulo','Morumbi')
-GO
+
+INSERT INTO Times VALUES('Botafogo-SP','Ribeirão Preto','Santa Cruz')
+INSERT INTO Times VALUES('Bragantino','Bragança Paulista','Nabi Abi Chedid')
+INSERT INTO Times VALUES('Corinthians','São Paulo','Arena Corinthians')
+INSERT INTO Times VALUES('Ferroviária','Araraquara','Fonte Luminosa')
+INSERT INTO Times VALUES('Guarani','Campinas','Brinco de Ouro da Princesa')
+INSERT INTO Times VALUES('Ituano','Itu','Novelli Júnior')
+INSERT INTO Times VALUES('Mirassol','Mirassol','Jóse Maria de Campos Maia')
+INSERT INTO Times VALUES('Novorizontino','Novo Horizonte','Jorge Ismael de Biasi')
+INSERT INTO Times VALUES('Oeste','Barueri','Arena Barueri')
+INSERT INTO Times VALUES('Palmeiras','São Paulo','Allianz Parque')
+INSERT INTO Times VALUES('Ponte Preta','Campinas','Moisés Lucarelli')
+INSERT INTO Times VALUES('Red Bull Brasil','Campinas','Moisés Lucarelli')
+INSERT INTO Times VALUES('Santos','Santos','Vila Belmiro')
+INSERT INTO Times VALUES('São Bento','Sorocaba','Walter Ribeiro')
+INSERT INTO Times VALUES('São Caetano','São Caetano do Sul','Anacletto Campenella')
+INSERT INTO Times VALUES('São Paulo','São Paulo','Morumbi')
+
+/*
+- Grupos (Coritnthians, Palmeiras, Santos e São Paulo NÃO PODEM estar no mesmo
+ grupo) (A coluna Grupo não pode aceitar nenhum valor diferente de A, B, C, D)
+ Grupos 
+	(Grupo | CodigoTime)
+*/
 CREATE TABLE Grupos(
 	Grupo			VARCHAR(1) CHECK (Grupo IN ('A','B','C','D')),
 	Codigo_Time		INT
 PRIMARY KEY (Grupo, Codigo_Time)
 )	
-GO
+
+/*
+- Jogos (A primeira fase ocorrerá em 12 datas seguidas, sempre rodada cheia (os 8
+ jogos), aos domingos e quartas) Jogos
+	(CodigoTimeA | CodigoTimeB | GolsTimeA | GolsTimeB | Data)
+*/
 CREATE TABLE Jogos(
 	CodigoTimeA		INT,
 	CodigoTimeB		INT,
@@ -44,16 +60,24 @@ CREATE TABLE Jogos(
 	DataJogo		DATE	CHECK(DataJogo BETWEEN '2019-01-19' and '2019-04-21')	
 PRIMARY KEY (CodigoTimeA, CodigoTimeB)
 )
-GO
+
+-- Chaves estrangeiras 
 ALTER TABLE Grupos ADD CONSTRAINT FK_Grupos_Times
 FOREIGN KEY(Codigo_Time) REFERENCES Times(CodigoTime)
-GO
+
 ALTER TABLE Jogos ADD CONSTRAINT FK_Jogos_TimesA
 FOREIGN KEY(CodigoTimeA) REFERENCES Times(CodigoTime)
-GO
+
 ALTER TABLE Jogos ADD CONSTRAINT FK_Jogos_TimesB
 FOREIGN KEY(CodigoTimeB) REFERENCES Times(CodigoTime)
 GO
+
+/*
+  O sistema deve se comportar da seguinte maneira:
+
+  - Uma tela deve chamar uma procedure que divide os times nos quatro grupos, preenchendo,
+  aleatoriamente (com exceção da regra já exposta em Grupos).
+*/
 CREATE PROC sp_gerarGrupos
 AS
     DECLARE @random INT, 
@@ -64,56 +88,72 @@ AS
             @count AS INT,
             @aux AS CHAR(1)
 
+	-- Define a quantidade de times em cada grupo:
+	-- Secundarios: 3 Vagas em cada grupo
     SET @secundarios = 'AAABBBCCCDDD'
+	-- Principais: 1 Vaga em cada grupo
     SET @principais = 'ABCD'
 
+	-- Contador de ID dos times
     SET @count = 1
 
+	-- Ira realizar o ciclo até ser definido o grupo dos 16 Times
     WHILE (@count <= 16) 
     BEGIN 
+		-- Define se ira pegar a vaga na lista principal ou secundaria
+		-- Se o time estiver na lista de "Principais" (Sp, Corint, Palm, Sant)
         IF ((@count = 3) OR (@count = 16) OR (@count = 10) OR (@count = 13)) 
         BEGIN 
             SET @time = @principais
             SET @aux = 'p'
         END 
+		-- Se estiver na lista de "Secundarios" 
         ELSE
         BEGIN 
             SET @time = @secundarios
             SET @aux = 's'
-        END 
+        END  
 
+		-- Define um grupo aleatóriamente
         SET @random = FLOOR(RAND()*(LEN(@time))+1)
+        -- Utilizando as vagas escolhidas anteriormente
+		SET @grupo = SUBSTRING(@time, @random, 1)
 
-        SET @grupo = SUBSTRING(@time, @random, 1)
+		-- Remove uma vaga do grupo escolhido
         SET @time = STUFF(@time, PATINDEX('%' + @grupo + '%', @time), LEN(@grupo), '')
 
+		-- Registra as vagas restantes 
         IF (@aux = 's')
         BEGIN 
-
             SET @secundarios = @time 
-
         END 
         ELSE 
         BEGIN
-
             SET @principais = @time
-
         END  
 
+		-- Insere na tabela Grupos o grupo sorteado juntamente com o ID do time
         INSERT INTO Grupos VALUES (@grupo, @count)
+
+		-- Passa para o próximo time
         SET @count = @count + 1 
 
         END 
 GO
-EXEC sp_gerarGrupos
-go
-select g.Grupo, t.NomeTime
-from Grupos g
-inner join Times t
-on t.CodigoTime = g.Codigo_Time
-select * from Times
-select * from Jogos
-GO
+
+
+/*
+	- Uma tela deve gerar as rodadas dos jogos, de acordo com as regras do
+	campeonato, preenchendo a tabela jogos.
+
+	Lembre-se, cada rodada tem 8 jogos (todos os 16 times). Lembre-se também que, as rodadas
+	vão acontecer de quarta e domingo, sucessivamente, sem pausas.
+
+	• Um jogo não pode ocorrer 2 vezes, mesmo em rodadas diferentes
+	• Um time não pode aparecer 2 vezes na mesma rodada
+	• A fase de grupos vai terminar antes da data final do campeonato, uma vez que o
+	  campeonato prevê datas das fase eliminatórias também
+*/
 CREATE PROC sp_gerarJogos
 AS
 	DECLARE @dia_de_hoje AS DATE, 
@@ -133,7 +173,7 @@ AS
 	-- Enquanto o campeonato estiver rolando 
 	WHILE (@dia_de_hoje < @dia_final)
 	BEGIN 
-		-- Verifica se Ã© dia de Jogo (Quarta ou Domingo)
+		-- Verifica se é dia de Jogo (Quarta ou Domingo)
 		IF ((DATEPART(WEEKDAY, @dia_de_hoje) = 1) OR (DATEPART(WEEKDAY, @dia_de_hoje) = 4))
 		BEGIN 
 
@@ -153,25 +193,25 @@ AS
 									 @id_time = j.CodigoTimeB) AND
 									 @dia_de_hoje = j.DataJogo))
 
-				-- Caso ainda nÃ£o tenha jogado 
+				-- Caso ainda não tenha jogado 
 				IF (@codigo IS NULL)
 				BEGIN
 					SET @jogou = 0
 					SET @contador = 1
 					SET @adversario = 0
 
-					-- Em quanto ainda nÃ£o jogou e ainda tem adversÃ¡rios para serem enfrentados
+					-- Em quanto ainda não jogou e ainda tem adversários para serem enfrentados
 					WHILE ((@jogou = 0) AND (@contador < 16))
 					BEGIN 
 						
-						-- Escolhe o adversÃ¡rio
+						-- Escolhe o adversário
 						SET @adversario = @id_time + @contador 
 						IF (@adversario > 16)
 						BEGIN
 							SET @adversario = @adversario - 16 
 						END 
 
-						-- Verifica se adversario jÃ¡ jogou no dia de hoje
+						-- Verifica se adversario já jogou no dia de hoje
 						SET @codigoAdv = NULL
 						SET @codigoAdv = (SELECT j.CodigoTimeA 
 										  FROM Jogos AS j 
@@ -179,20 +219,20 @@ AS
 												 @adversario = j.CodigoTimeB) AND
 												 @dia_de_hoje = j.DataJogo))
 
-						-- Verfica se ambos os times jÃ¡ jogaram um contra o outro				
+						-- Verfica se ambos os times já jogaram um contra o outro				
 						SET @codigo = NULL
 						SET @codigo = (SELECT j.CodigoTimeA
 									   FROM Jogos AS J 
 									   WHERE (j.CodigoTimeA = @id_time AND j.CodigoTimeB = @adversario) OR 
 											 (j.CodigoTimeA = @adversario AND j.CodigoTimeB = @id_time))
 						
-						-- Se alguma das condiÃ§Ãµes forem Verdadeiras, ira se decidir um novo adversario.
+						-- Se alguma das condições forem Verdadeiras, ira se decidir um novo adversario.
 						IF ((@codigo IS NOT NULL) OR (@codigoAdv IS NOT NULL) or (@id_time = @adversario))
 						BEGIN 
 							SET @contador = @contador + 1
 						END 
 
-						-- SenÃ£o eles irÃ£o se enfrentar 
+						-- Senão eles irão se enfrentar 
 						ELSE 
 						BEGIN 
 							SET @jogou = 1; 
@@ -208,13 +248,64 @@ AS
 
 	END 
 GO
+
+
+--	- Uma tela deve mostrar 4 Tabelas com os 4 grupos formados.
+CREATE FUNCTION fn_gerarTabelaGrupo(@grupo AS CHAR(1))
+RETURNS @table TABLE (
+cod_time	INT,
+nome_time	VARCHAR(100)
+)
+AS
+BEGIN
+
+	INSERT INTO @table 
+		SELECT t.CodigoTime, t.NomeTime
+		FROM Grupos g, Times t
+		WHERE t.CodigoTime = g.Codigo_Time
+			AND Grupo = @grupo
+
+	RETURN 
+END 
+GO
+
+
+--	- Uma tela deve mostrar um Campo, onde o usuário digite a data e, em caso de ser uma data com
+--	rodada, mostre uma tabela com todos os jogos daquela rodada.
+CREATE FUNCTION fn_consultarData (@verfData DATE)
+RETURNS @table TABLE (
+nome_timeA	VARCHAR(100),
+nome_timeB	VARCHAR(100)
+)
+AS
+BEGIN 
+	INSERT INTO @table 
+		SELECT ta.nomeTime, tb.NomeTime
+		FROM Jogos j, Times ta, Times tb
+		WHERE ta.CodigoTime = j.CodigoTimeA
+			AND tb.CodigoTime = j.CodigoTimeB
+			AND j.DataJogo = @verfData
+	RETURN 
+END 
+GO 
+
+-- SELECTS -- 
+SELECT g.Grupo, t.NomeTime
+FROM Grupos g
+INNER JOIN Times t
+ON t.CodigoTime = g.Codigo_Time
+
+SELECT * FROM Times 
+SELECT * FROM Jogos ORDER BY DataJogo
+
+-- PROCEDURES -- 
+EXEC sp_gerarGrupos
 EXEC sp_gerarJogos
 
+-- FUNCTIONs -- 
+SELECT * FROM fn_gerarTabelaGrupo('A')
+SELECT * FROM fn_gerarTabelaGrupo('B')
+SELECT * FROM fn_gerarTabelaGrupo('C')
+SELECT * FROM fn_gerarTabelaGrupo('D')
 
-select * from Jogos
-order by CodigoTimeB
-
-
-
-
-
+SELECT * FROM fn_consultarData('2019-01-20')	
